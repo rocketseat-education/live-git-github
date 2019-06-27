f44b57a9ca75aaf9bfa7ef069fdb5648f7831a72
Henrique Martins <enrque.ns@gmail.com>
2019-06-27 20:58:36 -0300

    feature: new route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (_, res) => {
+  return res.json({ hello: 'root' })
+});
+
 app.get('/teste', (req, res) => {
   return res.json({ hello: 'world 2' })
 });
